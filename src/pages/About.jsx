3cf6cd2a05300398ce424461ffb93fbe2edebb28
bapr5 <venediktov@itain.ru>
2024-07-31 18:01:16 +0300

    socketio

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,52 +1,51 @@
-import { React, useState, useEffect } from 'react'
+import { React, useState, useEffect, useCallback } from 'react'
 import { ConnectionState } from '../components/ConnectionState';
+import LogBox from '../components/LogBox'
+// import { socket } from '../socket'
+import { io } from 'socket.io-client';
+let SOCK_URL = '127.0.0.1:3005'
+const socket = io(SOCK_URL)
 
-
-// import { io } from 'socket.io-client'
-// const socket = io()
-
-import {socket} from '../socket'
-
+function onButton() {
+  socket.emit("hi");
+  console.log("hi");
+}
 export default function About() {
   const [connections, setConnections] = useState([])
   const [socketStatus, setSocketStatus] = useState(socket.connected);
-
   useEffect(() => {
+
+
+
     function onConnect() {
-      
       setSocketStatus(true);
     }
 
     function onDisconnect() {
       setSocketStatus(false);
     }
-
-    socket.on("connection",(socket)=>{
-      setConnections(socketStatus+socket)
+    socket.on("connect", () => {
+      console.log(socket.id)
       onConnect()
     })
-    // socket.on('connect', onConnect);
     socket.on('disconnect', onDisconnect);
-
+    socket.on('number', (number) => {
+      console.log(number)
+    })
     return () => {
       socket.off('connect', onConnect);
       socket.off('disconnect', onDisconnect);
     };
   }, []);
-
-  // const [isConnected, setIsConnected] = useState('ocket.connected');
-  
-  // const io = require('socket.io')
-  // const socket = io('http://localhost'); 
   return (
-<>
-<div>
-        <ConnectionState isConnected={ ConnectionState } />
-
-    </div>
-    <div>
-      <i>{connections}</i>
-    </div>
-</>
+    <>
+      <div>
+        <ConnectionState isConnected={ConnectionState} />
+      </div>
+      <div>
+        <button onClick={() => {  onButton(); }}>Нажми меня</button>
+        <i>{connections}</i>
+      </div>
+    </>
   )
 }
--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-let SOCK_URL='127.0.0.1:3333'
+let SOCK_URL='127.0.0.1:3005'
 
 export const socket = io(SOCK_URL)
